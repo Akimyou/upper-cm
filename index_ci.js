191ef2d648eaa1d5d6fdbdffeccec518c5f5db7c
mingming <changming.ma@reorientgroup.com>
2018-03-07 19:23:37 +0800

    Update

--- index_ci.js
+++ index_ci.js
@@ -1,10 +1,22 @@
+var crossEnv = require('cross-env')
+
 var upperCm = require('./index.js')
 
+var args = process.argv.slice(2)
+var argsOther = process.argv.slice(4)
+var argsNext = []
+
 var options = {
-  target: process.argv[2],
-  deep: process.argv[3]
+  target: args[0],
+  deep: args[1]
 }
 
 var result = upperCm.getPathNodeModules(options)
 
-console.log(result)
+argsNext.push('NODE_PATH=' + result)
+
+for (var i = 0; i < argsOther.length; i++) {
+  argsNext.push(argsOther[i])
+}
+
+crossEnv(argsNext)
